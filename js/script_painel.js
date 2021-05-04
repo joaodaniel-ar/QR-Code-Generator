79518940a03876c15ac2956fbddaf824e568dc88
João Daniel <joaodaniel.tn@gmail.com>
2021-05-04 11:25:50 -0300

    First Commit

---
+++ js/script_painel.js
@@ -0,0 +1,28 @@
+/* background squares */
+const ulSquares = document.querySelector("ul.squares");
+
+console.log("ulSquares: " + ulSquares);
+
+for (let i = 0; i < 21; i++) {
+  const li = document.createElement("li");
+  
+
+  const random = (min, max) => Math.random() * (max - min) + min;
+
+  const size = Math.floor(random(10, 120));
+  const position = random(1, 99);
+  const delay = random(1, 0.1);
+  const duration = random(24, 12);
+
+  li.style.width = `${size}px`;
+  li.style.height = `${size}px`;
+  li.style.bottom = `-${size}px`;
+
+  li.style.left = `${position}%`;
+
+  li.style.animationDelay = `${delay}s`;
+  li.style.animationDuration = `${duration}s`;
+  li.style.animationTimingFunction = `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`;
+
+  ulSquares.appendChild(li);
+}
